e523aaa2d3cf0f67d2e174476e4e951220e939fe
Ali Akrour <ali.akrour@gmail.com>
2020-12-11 14:53:10 +0100

    Update docker_run_git.sh
    install cmd exec by root

--- .docker/docker_run_git.sh
+++ .docker/docker_run_git.sh
@@ -55,7 +55,7 @@ if [ ! -f ./config/settings.inc.php ]; then
         fi
 
         echo "\n* Launching the installer script..."
-        runuser -g www-data -u www-data -- php /var/www/html/$PS_FOLDER_INSTALL/index_cli.php \
+        php /var/www/html/$PS_FOLDER_INSTALL/index_cli.php \
         --domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
         --db_password=$DB_PASSWD --prefix="$DB_PREFIX" --firstname="John" --lastname="Doe" \
         --password=$ADMIN_PASSWD --email="$ADMIN_MAIL" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
